ff3b7be51ccaba6a56291013e9307bab330999fb
abderrahmane <abderrahmaneMustapha030898@gmail.com>
2021-07-31 15:28:34 +0100

    feat :  add a date to models

--- backend/src/keys/models.js
+++ backend/src/keys/models.js
@@ -6,8 +6,8 @@ const Schema = mongoose.Schema
 const Key  = new Schema({
     id : {type : String, unique:[ true]},
     name : { type : String,unique:[ true, 'name already exists'], required:[true, ' this field is required']},
-    key : { type: String, unique:[ true, 'key already exists'],  required : [true, ' this field is required']},
-})
+    key : { type: String, unique:[ true, 'key already exists'],  required : [true, ' this field is required']}, 
+}, { timestamps: { createdAt: 'created_at' } })
 
 key = mongoose.model("Key", Key)
 module.exports =  {key}
