d2490a4c510113628fcfb8922864f69f7bcbe91d
abderrahmane <abderrahmaneMustapha030898@gmail.com>
2021-08-01 13:23:33 +0100

    fix :  ui

--- phone/screens/search/ui/search-ui.js
+++ phone/screens/search/ui/search-ui.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { View, FlatList } from 'react-native';
-import { List, SearchBar, Button, Popover, Icon, Modal, Toast, InputItem} from '@ant-design/react-native';
+import { List, SearchBar, Button, Popover, Icon, Modal, Toast, InputItem } from '@ant-design/react-native';
 
 // this component show keys , and play the role of a main component for
 // this page
@@ -12,22 +12,26 @@ export default function Search({ styles }) {
   const [query, setQuery] = useState()
 
 
-
+  const fetchData = async () => {
+    console.log(query)
+    await fetch(`${URL}api/keys?query=${query}`)
+      .then(response => response.json())
+      .then(data => {
+        
+        setItems(data)
+        
+      })
+  }
   useEffect(() => {
-    const fetchData = async () => {
-      await fetch(`${URL}api/keys?query=${query}`)
-        .then(response => response.json())
-        .then(data => {
-          setItems(data)
-        })
-    }
+   
     fetchData()
   }, [query])
 
   return (
     <View style={styles.center}>
-      <SearchBar  style={{marginBottom: "1em"}}placeholder="Search" cancelText="Cancel" onSubmit={(val) => {
+      <SearchBar showCancelButton={false} placeholder="Search"  onSubmit={(val) => {
         setQuery(val)
+        fetchData()     
       }} />
       <CModal styles={styles} />
 
@@ -75,7 +79,7 @@ const CPopover = ({ id }) => {
 // this a modal appear when a user want to add a key
 const CModal = ({ styles }) => {
   const [visible, setVisible] = useState(false)
-  const [keyInput, setKeyInput]  =  useState()
+  const [keyInput, setKeyInput] = useState()
   const handleVisibleChange = () => {
     setVisible(!visible)
     console.log("aaa")
@@ -93,9 +97,9 @@ const CModal = ({ styles }) => {
       .then(response => response.json())
       .then(data => {
         if (data.error)
-        Toast.show(data.error)
-        else 
-        Toast.show("data saved succesfully")
+          Toast.show(data.error)
+        else
+          Toast.show("data saved succesfully")
       })
   }
   return (
@@ -112,21 +116,21 @@ const CModal = ({ styles }) => {
         visible={visible}
         closable={true}
         onClose={handleVisibleChange}
-      
+
         title="Add a key you only need to enter a name"
       >
-        <div style={{ height: '50%', padding:"2em" }}>
-        <InputItem
+        <div style={{ height: '50%', padding: "2em" }}>
+          <InputItem
             clear
-            value={keyInput}        
+            value={keyInput}
             placeholder="Enter a key name"
-            onChange={(val)=>{
+            onChange={(val) => {
               setKeyInput(val)
             }}
           >
           </InputItem>
-          <Button style={{marginTop:"0.5em", marginBottom:"0.5em" }}  onPress={() => {
-           
+          <Button style={{ marginTop: "0.5em", marginBottom: "0.5em" }} onPress={() => {
+
             addKey()
             handleVisibleChange()
           }} type="primary">Add a key</Button>
